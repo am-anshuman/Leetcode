ad85ce31c6f791f8e3ed0a606ba4a3a089cba650
am-anshuman <anshumansahu0310@gmail.com>
2023-04-09 08:43:31 +0530

    Added Two Sum Folder
    Added Two Sum Folder with O(n^2) solution.

---
+++ Two Sum/twoSum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target) {
+    vector<int> x;
+
+    for(int i = 0; i < nums.size(); i++) {
+        for(int j = i+1; j < nums.size(); j++) {
+            if(nums[i] + nums[j] == target) {
+                x.push_back(i);
+                x.push_back(j);
+            }
+        }
+    }
+    return x;
+}

1de55923aca00483f5546567c8cb7ac9dde8bb02
am-anshuman <anshumansahu0310@gmail.com>
2023-04-10 19:32:19 +0530

    Made changes
    Added pattern 13 to 18

--- Patterns/Pattern.cpp
+++ Patterns/Pattern.cpp
@@ -141,10 +141,87 @@ void print12(int n) {
     }
 }
 
+void print13(int n) {
+    int num = 1;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << "\n";
+    }
+}
+
+void print14(int n) {
+    for(int i = 1; i <= n; i++) {
+        int ascii_char = 65;
+        for(int j = 1; j <= i; j++) {
+            cout << char(ascii_char) << " ";
+            ascii_char++;
+        }
+        cout << "\n";
+    }
+}
+
+void print15(int n) {
+    for(int i = 1; i <= n; i++) {
+        int ascii_char = 65;
+        for(int j = n; j >= i; j--) {
+            cout << char(ascii_char) << " ";
+            ascii_char++;
+        }
+        cout << "\n";
+    }
+}
+
+void print16(int n) {
+    int ascii_char = 65;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << char(ascii_char) << " ";
+        }
+        cout << "\n";
+        ascii_char++;
+    }
+}
+
+void print17(int n) {
+    for(int i = 0; i < n; i++) {
+        int ascii_char = 65;
+        // space
+        for(int j = 0; j < n-i-1; j++) {
+            cout << " ";
+        }
+        // character
+        for(int k = 0; k < 2*i+1; k++) {
+            cout << char(ascii_char);
+            if(k < i) {ascii_char++;}
+            else {ascii_char--;}
+        }
+        // space
+        for(int l = 0; l < n-i-1; l++) {
+            cout << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void print18(int n) {
+    int ascii_char = 65 + n - 1;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < i; j++) {
+            cout << char(ascii_char) << " ";
+            ascii_char++;
+        }
+        ascii_char = ascii_char - i - 1;
+        cout << "\n";
+    }
+}
+
 int main() {
     int t; cin >> t;
     while(t--) {
         int n; cin >> n;
-        print12(n);
+        print18(n);
     }
 }
